0418be05fc45d1fc86d42853d455b3328b1b21a8
newhorizon-tech <alaukikinbox@gmail.com>
2021-10-08 07:44:06 +0530

    Add input for new scores

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import './style.scss';
 import display from './display';
 
-display();
+import { postData } from './api-interface';
+
+const newScoreBtn = document.querySelector('#new-score');
+const scoreInput = document.querySelector('#score');
+const nameInput = document.querySelector('#name');
+const refreshBtn = document.querySelector('#refresh');
+
+const clearInput = () => {
+  nameInput.value = '';
+  scoreInput.value = '';
+};
+
+newScoreBtn.addEventListener('click', () => {
+  const newScore = scoreInput.value;
+  const newName = nameInput.value;
+
+  const scoreObj = {
+    user: newName,
+    score: newScore,
+  };
+  postData(scoreObj);
+  clearInput();
+});
+
+refreshBtn.addEventListener('click', display);
+
+document.addEventListener('DOMContentLoaded', display);
